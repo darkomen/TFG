bf4d52b3fb5bc2defc974faf7a6ba9fc0b5be4a2
Santi <santiago.lopez.pina@gmail.com>
2015-08-13 17:04:39 +0200

    Cambio de velocidades

--- programacion/TRACTORA/TRACTORA.ino
+++ programacion/TRACTORA/TRACTORA.ino
@@ -5,7 +5,7 @@
 #include "TimerOne.h"
 
 //Modo debug para mostrar por pantalla información de la velocidad
-//#define DEBUG
+#define DEBUG
 
 //definiciones pines driver paso a paso
 #define PIN_STEP 46
@@ -26,9 +26,8 @@ long interval = 250; //intervalo de ejecución del programa en millis
 int ticks=0; //Variable con el valor de ticks que debemos contabilizar.
 int tick_count=0; //Variable donde vamos acumulano las interrupciones del timer.
 //BUENA
-//float rpm_speed[]= {2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9};//array con las velocidades en RPM
 float rpm_speed[]= {1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4};//array con las velocidades en RPM
-
+//float rpm_speed[]= {1.5, 1.7, 1.9, 2.1, 2.3, 2.5, 2.7, 2.9, 3.1, 3.3, 3.5, 3.7, 3.9, 4.1, 4.3, 4.5, 4.7, 4.9, 5.1, 5.3};
 
 //float rpm_speed[] ={  1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9};
 int tick_speed[velocidades+1] = {-1,0,0,0,0,0,0,0,0,0,0};  //tabla con los ticks de retardo, velocidades en RPM
